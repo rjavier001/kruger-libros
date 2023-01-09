c4e444ddc363e3a822e2c478fc2b3f3c4cbd9987
Javier Vallejo <rjavier001@gmail.com>
2023-01-08 21:44:01 -0500

    commponents added, pages added, temp api added, store added

--- book-app/src/App.js
+++ book-app/src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import Index from "./pages/index";
+import Create from "./pages/create";
+import View from "./pages/view";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Store from "./store/store";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Store>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="create" element={<Create />} />
+          <Route path="view/:bookId" element={<View />} />
+        </Routes>
+      </div>
+    </Store>
   );
 }
 
